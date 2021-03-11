ec5567e8485df21b0e30067dc16cc17ce91f67f2
Jumoc <juanpcaicedo2@gmail.com>
2021-03-11 00:24:11 -0500

    Fix change syntax

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,31 @@
 #include "binary_trees.h"
 
+/**
+ * max - calcultes the max between two numbers
+ *
+ * @a: number a
+ * @b: number b
+ *
+ * Return: the max
+ */
+int max(int a, int b)
+{
+	return (a >= b ? a : b);
+}
+
+/**
+ * height - calcultes the height of a given tree
+ *
+ * @tree: tree to operate
+ *
+ * Return: amount of leaves
+ */
+int height(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (1 + max(height(tree->left), height(tree->right)));
+}
 
 /**
  * binary_tree_balance - returns the amounts of leaves on a given tree
@@ -10,13 +36,14 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int height_l;
-	int height_r;
+	int left;
+	int right;
 
 	if (!tree)
 		return (0);
 
-	height_l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
-	return (height_l - height_r);
+	left = height(tree->left);
+	right = height(tree->right);
+	printf("left: %d - right: %d", left, right);
+	return (left - right);
 }
